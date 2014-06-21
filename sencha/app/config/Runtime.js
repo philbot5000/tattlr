b5f2cd30e9244b5f00f46f2ada5270dd65c94459
philbot5000 <philbot5000@gmail.com>
2014-06-21 13:58:24 -0600

    First commit.

---
+++ sencha/app/config/Runtime.js
@@ -0,0 +1,11 @@
+Ext.define('FP.config.Runtime', {
+	singleton: true,
+	config: {
+		currentUserId: null,
+		account: null,
+		userAccount: null,
+		transaction: null,
+		accountBalance: null
+	}
+	
+});
---
+++ sencha/build/temp/package/FamilyPenny/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/philmerrell/Sites/Tattlr/sencha/app.js
